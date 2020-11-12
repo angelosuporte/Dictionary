9ae6ca46298151eeb769701c4692158d81b064e6
Angelo Carvalho <angelosuporteti@gmail.com>
2020-11-12 14:36:50 -0300

    Testando funcionamento básico do Dictionary

--- DictionarySortedDictionary/Program.cs
+++ DictionarySortedDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionarySortedDictionary
 {
@@ -6,7 +7,28 @@ namespace DictionarySortedDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "Maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99712234";
+
+            cookies.Remove("phone");
+      
+            Console.WriteLine("Usuário: " + cookies["user"]);
+            Console.WriteLine("Email:   " + cookies["email"]);
+            if (cookies.ContainsKey("phone"))
+            {
+                Console.WriteLine("Phone:   " + cookies["phone"]);
+            }
+            else
+            {
+                Console.WriteLine("A chave 'phone' não existe");
+            }
+
+
+            Console.WriteLine("Size: " + cookies.Count);
+            Console.ReadKey();
         }
     }
 }
