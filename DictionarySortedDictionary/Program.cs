b3a8b3a09cb3adc301eb17f7a9c28857d64b6587
Angelo Carvalho <angelosuporteti@gmail.com>
2020-11-12 13:34:44 -0300

    Adicionar arquivos de projeto.

---
+++ DictionarySortedDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DictionarySortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
