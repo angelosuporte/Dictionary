282a5d025c5acc0d1626b9eddfbd7da8415e1a51
Angelo Carvalho <angelosuporteti@gmail.com>
2020-11-13 19:23:26 -0300

    Exercicio sobre Dictionary realizado com sucesso

--- DictionarySortedDictionary/Program.cs
+++ DictionarySortedDictionary/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace DictionarySortedDictionary
 {
@@ -7,12 +8,45 @@ namespace DictionarySortedDictionary
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> candidatos = new Dictionary<string, string>();
+            Dictionary<string, int> candidatos = new Dictionary<string, int>();
 
             Console.Write("Informe o path completo: ");
             string path = Console.ReadLine();
 
+            try
+            {
+                using (StreamReader abrirArquivo = File.OpenText(path))
+                {
+                    while (!abrirArquivo.EndOfStream)
+                    {
+                        string[] linha = abrirArquivo.ReadLine().Split(',');
+                        string nome = linha[0];
+                        int votos = int.Parse(linha[1]);
 
+                        if (candidatos.ContainsKey(nome))
+                        {
+                            candidatos[nome] += votos;
+                        }
+                        else
+                        {
+                            candidatos[nome] = votos;
+                        }
+                    }
+
+                    Console.WriteLine("Resultado: ");
+                    foreach (var candidato in candidatos)
+                    {
+                        Console.WriteLine(candidato.Key + ": " + candidato.Value);
+                    }
+                    
+                    Console.ReadKey();
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
